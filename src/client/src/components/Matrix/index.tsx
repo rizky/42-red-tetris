ab460e560ff2c07a060bab63bfaad350b9568f67
Rizky Ario <rizkyario@me.com>
2020-09-23 22:16:14 +0200

    POC tetris engine

--- src/client/src/components/Matrix/index.tsx
+++ src/client/src/components/Matrix/index.tsx
@@ -4,34 +4,12 @@ import * as React from 'react';
 
 import Block from '/components/Block';
 
-const Matrix = (): JSX.Element => {
-  const matrix = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
-    [0, 0, 1, 1, 1, 0, 1, 1, 1, 0],
-    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-    [1, 1, 0, 0, 1, 0, 1, 1, 1, 0],
-  ];
+const Matrix = ({ matrix }: { matrix: Matrix }): JSX.Element => {
   return (
     <View style={{ height: _.size(matrix) * 22, padding: 2, justifyContent: 'space-between', borderColor: 'black', borderWidth: 1, backgroundColor: '#9ead86' }}>
-      {_.map(matrix, (row) =>
-        <View style={{ flexDirection: 'row', width: _.size(row) * 22, justifyContent: 'space-between' }}>
-          {_.map(row, (value) => <Block value={value} />)}
+      {_.map(matrix, (row, index) =>
+        <View key={index} style={{ flexDirection: 'row', width: _.size(row) * 22, justifyContent: 'space-between' }}>
+          {_.map(row, (value, index) => <Block key={index} value={value} />)}
         </View>)}
     </View>
   );
--- src/client/src/screens/Playground/index.tsx
+++ src/client/src/screens/Playground/index.tsx
@@ -1,13 +1,49 @@
-import * as React from 'react';
+import _ from 'lodash';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import useInterval from '@use-it/interval';
+import useKey from 'use-key-hook';
 
 import Matrix from '/components/Matrix';
+import { blankMatrix } from '/constants/tetriminos';
+import Block from '/models/block';
+import { blockType } from '/constants/tetriminos';
+
+const printBlock = (matrix: Matrix, block: Block) => { 
+  const { shape, xy } = block;
+  shape.forEach((m, i) => (
+    m.forEach((n, j) => {
+      if (xy[0] + i >= 0 && xy[0] + i < 20 && xy[1] + j < 10) {
+        matrix[xy[0] + i][xy[1] + j] = shape[i][j];
+      }
+    })
+  ));
+  return matrix;
+};
 
 export default function Playground(): JSX.Element {
+  const [block, setBlock] = React.useState<Block>(new Block({
+    type: _.sample<TetriminosType>(blockType) ?? 'T',
+  }));
+  useInterval(() => {
+    setBlock((currentBlock) =>
+      (currentBlock.xy[0] + currentBlock.shape.length < 20)
+        ? currentBlock.fall()
+        : new Block({
+          type: _.sample<TetriminosType>(blockType) ?? 'T',
+        }));
+  }, 500);
+  useKey((key: number) => {
+    if (key === 38) setBlock((currentBlock) => currentBlock.rotate());
+    if (key === 37) setBlock((currentBlock) => currentBlock.left());
+    if (key === 39) setBlock((currentBlock) => currentBlock.right());
+    if (key === 40) setBlock((currentBlock) => currentBlock.fall());
+  });
+  const newMatrix = printBlock(blankMatrix(), block);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{'Playground'}</Text>
-      <Matrix />
+      <Matrix matrix={newMatrix} />
     </View>
   );
 }
