0c07562eb59e75a4b9f69a51ea5f5469c240269d
mrakhman <robinbad1312@yandex.ru>
2021-03-26 17:46:57 +0100

    Turn Block class to functions

--- src/client/components/Matrix/index.tsx
+++ src/client/components/Matrix/index.tsx
@@ -3,17 +3,17 @@ import { View, ViewStyle, StyleSheet } from 'react-native';
 import * as React from 'react';
 
 import BlockComponent from '/client/components/Block';
-import Block from '/client/models/block';
+import { isBlock } from '/client/controllers/blockControllers';
 import { cellState } from '/client/constants/tetriminos.ts';
 
 const blockWidth = 22;
 
-const Matrix = ({ block, matrix, style }: { block: Block, matrix: Matrix, style?: ViewStyle }): JSX.Element => {
+const Matrix = ({ block, matrix, style }: { block: BlockType, matrix: Matrix, style?: ViewStyle }): JSX.Element => {
   return (
     <View style={[styles.container, { height: _.size(matrix) * blockWidth }, style]}>
       {_.map(matrix, (row, rowIndex) =>
         <View key={rowIndex} style={{ flexDirection: 'row', width: _.size(row) * blockWidth, justifyContent: 'space-between' }}>
-          {_.map(row, (value, colIndex) => <BlockComponent key={colIndex} value={block.isBlock([rowIndex, colIndex]) ? cellState.OCCUPIED : value} />)}
+          {_.map(row, (value, colIndex) => <BlockComponent key={colIndex} value={isBlock(block, [rowIndex, colIndex]) ? cellState.OCCUPIED : value} />)}
         </View>)}
     </View>
   );
--- src/types/Tetriminos.d.ts
+++ src/types/Tetriminos.d.ts
@@ -2,6 +2,13 @@ type Matrix = number[][];
 
 type TetriminosType = 'I' | 'L' | 'J' | 'Z' | 'S' | 'O' | 'T';
 
+type BlockType  = {
+  pos: number[],
+  shape: number[][],
+  type: TetriminosType,
+  timeStamp: number,
+};
+
 type BlockOption = {
   pos?: number[],
   shape?: number[][],
