a3180f73a34cc76fa6ccc5e49dddc5720430bbbd
mrakhman <robinbad1312@yandex.ru>
2021-04-20 15:22:08 +0200

    No scroll on space button click

--- App.tsx
+++ App.tsx
@@ -5,6 +5,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './src/client/navigation';
 
 export default function App(): JSX.Element | null {
+  // Prevent window from scrolling on Space button click
+  window.addEventListener('keydown', function(e) {
+    if(e.keyCode === 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+  });
+
   return (
     <SafeAreaProvider>
       <Navigation />
