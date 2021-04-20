80b29130253af23a1a1d70da32c6c63e2b1f16a4
mrakhman <robinbad1312@yandex.ru>
2021-04-20 13:20:09 +0200

    Fix chat bugs

--- src/client/components/Chat/index.tsx
+++ src/client/components/Chat/index.tsx
@@ -1,7 +1,9 @@
 import 'react-chat-widget/lib/styles.css';
-import { Widget as ChatWidget, addResponseMessage } from 'react-chat-widget';
+import { Widget as ChatWidget, addResponseMessage, isWidgetOpened, dropMessages } from 'react-chat-widget';
 
 export {
   ChatWidget,
   addResponseMessage,
+  isWidgetOpened,
+  dropMessages,
 };
--- src/client/hooks/useKeyEvent/index.tsx
+++ src/client/hooks/useKeyEvent/index.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from 'react';
 import useKey from 'use-key-hook';
 import _ from 'lodash';
+import { isWidgetOpened } from '/client/components/Chat';
 
 import { blankMatrix } from '/client/constants/tetriminos';
 import { blockTypes } from '/client/constants/tetriminos';
@@ -23,6 +24,8 @@ export const useKeyEvent = ({ setIsPause, setMatrix, setBlock }
     setBlock: Dispatch<SetStateAction<BlockType>>,
   }): void => {
   useKey((_key: number, { keyCode }: { keyCode: number }) => {
+    // When Chat widget is opened, key press should not affect movement on Matrix Playground
+    if (isWidgetOpened()) return;
     if (keyCode === keyboard.reset) {
       setBlock(blockCreate({ type: _.sample(blockTypes) ?? 'T' }));
       setMatrix(blankMatrix);
